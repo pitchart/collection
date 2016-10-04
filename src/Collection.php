faf228ddc1dace7799ed30bc837b6f147e7e9a3d
Julien VITTE <j.vitte@alienor.net>
2016-10-04 14:24:36 +0200

    [TASK] phpdoc

--- src/Collection.php
+++ src/Collection.php
@@ -6,39 +6,66 @@ class Collection implements \ArrayAccess, \Countable {
 
 	protected $items;
 
+	/**
+	 * @param array $items
+	 */
 	public function __construct(array $items)
 	{
 		$this->items = $items;
 	}
 
+	/**
+	 * @param array $items
+	 * @return static
+	 */
 	public static function from(array $items)
 	{
 		return new static($items);
 	}
 
-	public function each($callback)
+	/**
+	 * @param callable $callback
+	 */
+	public function each(callable $callback)
 	{
 		foreach ($this->items as $item) {
 			$callback($item);
 		}
 	}
 
-	public function map($callback)
+	/**
+	 * @param callable $callback
+	 * @return static
+	 */
+	public function map(callable $callback)
 	{
 		return new static(array_map($callback, $this->items));
 	}
 
-	public function filter($callback)
+	/**
+	 * @param callable $callback
+	 * @return static
+	 */
+	public function filter(callable $callback)
 	{
 		return new static(array_filter($this->items, $callback));
 	}
 
-	public function reject($callback)
+	/**
+	 * @param callable $callback
+	 * @return static
+	 */
+	public function reject(callable $callback)
 	{
 		return new static(array_filter($this->items, !$callback));
 	}
 
-	public function reduce($callback, $initial)
+	/**
+	 * @param callable $callback
+	 * @param mixed $initial
+	 * @return mixed
+	 */
+	public function reduce(callable $callback, $initial)
 	{
 		$accumulator = $initial;
 
@@ -48,16 +75,28 @@ class Collection implements \ArrayAccess, \Countable {
 		return $accumulator;
 	}
 
+	/**
+	 * @param mixed $offset
+	 * @return bool
+	 */
 	public function offsetExists($offset)
 	{
 		return array_key_exists($offset, $this->items);
 	}
 
+	/**
+	 * @param mixed $offset
+	 * @return mixed
+	 */
 	public function offsetGet($offset)
 	{
 		return $this->items[$offset];
 	}
 
+	/**
+	 * @param mixed $offset
+	 * @param mixed $value
+	 */
 	public function offsetSet($offset, $value)
 	{
 		if ($offset === null) {
@@ -68,16 +107,25 @@ class Collection implements \ArrayAccess, \Countable {
 		}
 	}
 
+	/**
+	 * @param mixed $offset
+	 */
 	public function offsetUnset($offset)
 	{
 		unset($this->items[$offset]);
 	}
 
+	/**
+	 * @return int
+	 */
 	public function count()
 	{
 		return count($this->items);
 	}
 
+	/**
+	 * @return array
+	 */
 	public function toArray()
 	{
 		return $this->items;
