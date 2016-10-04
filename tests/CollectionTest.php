d635c74c739b2c80c8b94ae9d805e2c14b172425
Julien VITTE <vitte.julien@gmail.com>
2016-10-04 20:45:41 +0200

    [FEATURE] adds tests

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -44,6 +44,24 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expected, $collection->reduce($reducer, $initial));
     }
 
+    /**
+     * @param array $items
+     * @param callback $callback
+     * @param mixed $expected
+     * @dataProvider filterTestProvider
+     */
+    public function testCanBeFiltered($items, $callback, $expected) {
+        $collection = new Collection($items);
+        $this->assertEquals($expected, array_values($collection->filter($callback)->toArray()));
+    }
+
+    /**
+     * @dataProvider mapTestProvider
+     */
+    public function testCanMapDatas(array $items, $callback, array $expected) {
+        $this->assertEquals($expected, Collection::from($items)->map($callback)->toArray());
+    }
+
     public function countTestProvider() {
         return [
             'An empty array' => [[], 0],
@@ -55,6 +73,22 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
         return [
             'Sum reducing' => [[1,2,3,4], function($accumulator, $item) {return $accumulator + $item;}, 0, 10],
             'String reducing' => [['banana', 'apple', 'orange'], function($accumulator, $item) {return trim($accumulator.', '.$item, ', ');}, '', 'banana, apple, orange'],
+        ];
+    }
+
+    public function mapTestProvider() {
+        return [
+            'Add 1 mapper' => [[1,2,3], function($item) {return $item + 1;}, [2, 3, 4]],
+            'Concat mapper' => [['test1', 'test2', 'test3'], function($item) { return $item.'1';}, ['test11', 'test21', 'test31']],
+            'Empty data mapper' => [[], function($item) { return $item + 1;}, []],
+        ];
+    }
+
+    public function filterTestProvider() {
+        return [
+            'Pair filter' => [[1,2,3, 4], function($item) {return $item % 2 == 0;}, [2, 4]],
+            'String filter' => [['foo', 'bar', 'fizz', 'buzz'], function($item) { return strpos($item, 'f') !== false;}, ['foo', 'fizz']],
+            'Empty data mapper' => [[], function($item) { return $item + 1;}, []],
         ];
     }
 
