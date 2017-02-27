9c2a9b0c0564897a5bfb3180d781613a518e2f1b
Julien VITTE <vitte.julien@gmail.com>
2017-02-27 15:18:22 +0100

    [FEATURE] adds helpers to provide mapping, filtering, merge and reducing to any iterable

--- src/Helpers/collection.php
+++ src/Helpers/collection.php
@@ -29,3 +29,96 @@ function generator($items)
 {
     return GeneratorCollection::from($items);
 }
+
+/**
+ * Applies a callable to the elements of an iterable list and returns a Collection
+ *
+ * @param  iterable $items
+ * @param  callable $callable The callable takes an element as parameter and returns a transformed result
+ *
+ * @return CollectionInterface
+ */
+function map($items, $callable)
+{
+    if (is_array($items)
+        || !($items instanceof CollectionInterface)
+    ) {
+        $items = Collection::from($items);
+    }
+    return $items->map($callable);
+}
+
+/**
+ * Filters elements of an iterable list and returns a Collection
+ *
+ * @param  iterable $items
+ * @param  callable $callable The callable takes an element as parameter and returns a boolean
+ *
+ * @return CollectionInterface
+ */
+function filter($items, $callable)
+{
+    if (is_array($items)
+        || !($items instanceof CollectionInterface)
+    ) {
+        $items = Collection::from($items);
+    }
+    return $items->filter($callable);
+}
+
+/**
+ * Rejects elements of an iterable list and returns a Collection
+ *
+ * @param  iterable $items
+ * @param  callable $callable The callable takes an element as parameter and returns a boolean
+ *
+ * @return CollectionInterface
+ */
+function reject($items, $callable)
+{
+    if (is_array($items)
+        || !($items instanceof CollectionInterface)
+    ) {
+        $items = Collection::from($items);
+    }
+    return $items->reject($callable);
+}
+
+/**
+ * Merge one or more iterables and returns a collection
+ *
+ * @param iterable $iterables
+ *
+ * @return CollectionInterface
+ */
+function merge(...$iterables)
+{
+    foreach ($iterables as $position => $iterable) {
+        if (is_array($iterable)
+            || !($iterable instanceof CollectionInterface)
+        ) {
+            $iterables[$position] = Collection::from($iterable);
+        }
+    }
+    /** @var CollectionInterface $first */
+    $first = array_shift($iterables);
+    return $first->merge(...$iterables);
+}
+
+/**
+ * Iteratively reduce an iterable to a single value using a callable
+ *
+ * @param callable $callable The callable takes an accumulator and an iterable element as parameters and returns a values which will be the accumulator of the next iteration
+ * @param mixed    $initial
+ *
+ * @return mixed
+ */
+function reduce($items, $callable, $initial)
+{
+    if (is_array($items)
+        || !($items instanceof CollectionInterface)
+    ) {
+        $items = Collection::from($items);
+    }
+    return $items->reduce($callable, $initial);
+}
--- tests/Unit/HelpersTest.php
+++ tests/Unit/HelpersTest.php
@@ -19,4 +19,94 @@ class HelpersTest extends \PHPUnit_Framework_TestCase
 		self::assertInstanceOf(GeneratorCollection::class, $collection);
 	}
 
+	public function testMapReturnsAMappedCollection() {
+		$collection = ch\map([1,2,3,4], function ($item) { return $item + 1; });
+		self::assertInstanceOf(Collection::class, $collection);
+		self::assertEquals([2,3,4,5], $collection->values());	
+	}
+
+	/**
+	 * @param  iterable $iterable
+	 * @dataProvider iterableProvider
+	 */
+	public function testMapHelperCanMapAnyIterable($iterable)
+	{
+		self::assertEquals([2, 3, 4, 5], ch\map($iterable, function ($item) { return $item + 1; })->values());
+	}
+
+	public function testFilterReturnsAFilteredCollection() {
+		$collection = ch\filter([1,2,3,4], function ($item) { return $item % 2 == 0; });
+		self::assertInstanceOf(Collection::class, $collection);
+		self::assertEquals([2,4], $collection->values());	
+	}
+
+	/**
+	 * @param  iterable $iterable
+	 * @dataProvider iterableProvider
+	 */
+	public function testFilterHelperCanFilterAnyIterable($iterable)
+	{
+		self::assertEquals([2, 4], ch\filter($iterable, function ($item) { return $item % 2 == 0; })->values());
+	}
+
+	public function testRejectReturnsAFilteredCollection() {
+		$collection = ch\reject([1,2,3,4], function ($item) { return $item % 2 == 0; });
+		self::assertInstanceOf(Collection::class, $collection);
+		self::assertEquals([1,3], $collection->values());	
+	}
+
+	/**
+	 * @param  iterable $iterable
+	 * @dataProvider iterableProvider
+	 */
+	public function testRejectHelperCanFilterAnyIterable($iterable)
+	{
+		self::assertEquals([1, 3], ch\reject($iterable, function ($item) { return $item % 2 == 0; })->values());
+	}
+
+	public function testReduceReturnsAReducedValue() {
+		$reduced = ch\reduce([1,2,3,4], function ($accumulator, $item) { return $accumulator + $item; }, 0);
+		self::assertEquals(10, $reduced);	
+	}
+
+	/**
+	 * @param  iterable $iterable
+	 * @dataProvider iterableProvider
+	 */
+	public function testReduceHelperCanReduceAnyIterable($iterable)
+	{
+		self::assertEquals(10, ch\reduce($iterable, function ($accumulator, $item) { return $accumulator + $item; }, 0));
+	}
+
+	/**
+	 * @param  iterable $iterable1
+	 * @param  iterable $iterable2
+	 * @param  iterable $iterable3
+	 * @dataProvider mergeIterableProvider
+	 */
+	public function testMergeHelperCanMergeAnyIterable($iterable1, $iterable2, $iterable3)
+	{
+		self::assertEquals([1, 2, 3, 4, 5, 6], ch\merge($iterable1, $iterable2, $iterable3)->values());
+	}
+
+	public function iterableProvider()
+	{
+		return [
+			'Array' => [[1, 2, 3, 4]],
+			'Collection' => [Collection::from([1, 2, 3, 4])],
+			'GeneratorCollection' => [GeneratorCollection::from([1, 2, 3, 4])],
+			'ArrayAccess' => [new \ArrayIterator([1, 2, 3, 4])],
+			'Iterator' => [new \IteratorIterator(new \ArrayIterator([1, 2, 3, 4]))],
+		];
+	}
+
+	public function mergeIterableProvider()
+	{
+		return [
+			[[1, 2], [3, 4], [5, 6]],
+			[[1, 2], Collection::from([3, 4]), GeneratorCollection::from([5, 6])],
+			[GeneratorCollection::from([1, 2]), new \ArrayIterator([3, 4]), [5, 6]],
+			[new \IteratorIterator(new \ArrayIterator([1, 2])), [3, 4], Collection::from([5, 6])],
+		];
+	}
 }
